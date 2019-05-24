50de4f056170870fb6e92c31376e62703a47ce6b
zacharyjarnagin <jarnagin.z@husky.neu.edu>
2019-05-24 12:50:12 -0400

    created admin user account to database. each object can now hold a file with a url

--- database/articleAudio/articleaudiodb/admin.py
+++ database/articleAudio/articleaudiodb/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import Audio, NoAudio
+
 # Register your models here.
+admin.site.register(Audio)
+admin.site.register(NoAudio)
--- database/articleAudio/articleaudiodb/models.py
+++ database/articleAudio/articleaudiodb/models.py
@@ -9,10 +9,19 @@ from django.db import models
 class NoAudio(models.Model):
     result_text = "File Not Found"
 
+    # To represent this object as a string
+    def __str__(self):
+        return self.result_text
+
 # Represents audio with a file attached to the object
 
 
 class Audio(models.Model):
     result_text = "File found"
     audio_info = models.FileField(upload_to='audio/', default="File Not Found")
-    name = models.CharField
+    name = models.CharField(max_length=200, default="No Name Found")
+    url = models.CharField(max_length=200, default="No URL Found")
+
+    # To represent this object as a string
+    def __str__(self):
+        return self.url
