8a8a3c175561f71a0161ca7b9221d259873eca1b
zacharyjarnagin <jarnagin.z@husky.neu.edu>
2019-05-24 15:13:56 -0400

    added simple views for database objects

--- database/articleAudio/articleaudiodb/urls.py
+++ database/articleAudio/articleaudiodb/urls.py
@@ -2,7 +2,11 @@ from . import views
 
 from django.urls import path
 
-
 urlpatterns = [
+    # ex: /articleaudiodb/
     path('', views.index, name='index'),
+    # ex: /articleaudiodb/5/
+    path('<int:audio_id>/', views.detail, name='detail'),
+    # ex: /articleaudiodb/5/file/
+    path('<int:audio_id>/file/', views.file, name='file')
 ]
--- database/articleAudio/articleaudiodb/views.py
+++ database/articleAudio/articleaudiodb/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Audio
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the articleaudiodb index.")
+    latest_question_list = Audio.objects.order_by('-name')[:5]
+    output = ', '.join([a.name for a in latest_question_list])
+    return HttpResponse(output)
+
+
+def detail(request, audio_id):
+    return HttpResponse("You're looking at audio %s." % audio_id)
+
+
+def file(request, audio_id):
+    response = "You're looking at the file of audio %s."
+    return HttpResponse(response % audio_id)
